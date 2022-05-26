bfc1229f28d732e281ef1820ea742fa1ed748086
SpinBoxx <quentin.mimault@orange.fr>
2022-05-26 19:58:16 +0200

    update some css contact page

--- src/page/template/contact.vue
+++ src/page/template/contact.vue
@@ -1,11 +1,10 @@
 <template>
   <section id="contact" class="bg-gradient-to-t from-blue-400 h-screen md:h-screen items-center flex flex-row mx-auto">
-    <div class="container flex flex-row mx-auto p-5 pt-20 gap-5 justify-center xl:w-1/2">
-      <span class="text-3xl w-2/3">If you want to contact me, <span class="text-blue-400">download</span> my CV to get my email adress or send me poke on <span class="text-blue-600">Linkedin 😉</span></span>
-      <a href="https://www.linkedin.com/in/mymimo/" class="w-1/3">
+    <div class="container flex flex-row te mx-auto p-5 pt-20 gap-5 justify-center xl:w-1/2">
+      <span class="text-3xl">If you want to contact me, <span class="text-blue-400">download</span> my CV to get my email adress or send me poke on <span class="text-blue-600">Linkedin 😉</span></span>
+      <a href="https://www.linkedin.com/in/mymimo/" class="w-1/4">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-20 text-blue-700 fill-current hover:text-blue-800 cursor-pointer" viewBox="0 0 448 512"><path fill-rule="evenodd" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
       </a>
-
     </div>
   </section>
 </template>
